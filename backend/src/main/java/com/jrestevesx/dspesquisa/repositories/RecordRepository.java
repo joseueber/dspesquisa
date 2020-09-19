9c6d910caa1c8343ccc5076f2033f5d5eb287187
Júnior Esteves <juniorbdesteves@gmail.com>
2020-09-19 04:56:25 -0300

    Ajuste no GET Records

--- backend/src/main/java/com/jrestevesx/dspesquisa/repositories/RecordRepository.java
+++ backend/src/main/java/com/jrestevesx/dspesquisa/repositories/RecordRepository.java
@@ -1,11 +1,21 @@
 package com.jrestevesx.dspesquisa.repositories;
 
+import java.time.Instant;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.jrestevesx.dspesquisa.entities.Record;
 
 @Repository
 public interface RecordRepository extends JpaRepository<Record, Long>{
+
+	@Query("SELECT r FROM Record r WHERE "
+			+ "(coalesce(:min, null) IS NULL OR r.moment >= :min) AND "
+			+ "(coalesce(:max, null) IS NULL OR r.moment <= :max)")
+	Page<Record> findByMoments(Instant min, Instant max, Pageable pageable);
 
 }

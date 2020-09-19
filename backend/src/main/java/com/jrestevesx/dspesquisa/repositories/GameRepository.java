441499ff8134d4214d73720554fad8f66ffe5e8b
Júnior Esteves <juniorbdesteves@gmail.com>
2020-09-19 03:10:55 -0300

    Criação de entidades e acesso a dados. Entities, Repositories

---
+++ backend/src/main/java/com/jrestevesx/dspesquisa/repositories/GameRepository.java
@@ -0,0 +1,9 @@
+package com.jrestevesx.dspesquisa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jrestevesx.dspesquisa.entities.Game;
+
+public interface GameRepository extends JpaRepository<Game, Long>{
+
+}
---
+++ backend/src/main/java/com/jrestevesx/dspesquisa/repositories/GenreRepository.java
@@ -0,0 +1,9 @@
+package com.jrestevesx.dspesquisa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jrestevesx.dspesquisa.entities.Genre;
+
+public interface GenreRepository extends JpaRepository<Genre, Long>{
+
+}
